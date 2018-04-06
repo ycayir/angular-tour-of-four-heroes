54cb8b89bb50921fc3a2c01b3a73d28c071dfa07
Yalcin Cayir <yalcincayir@gmail.com>
2018-04-06 11:51:10 -0400

    Added list of heroes. Added event handler to list.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Spider-Man' },
+    { id: 12, name: 'Iron Man' },
+    { id: 13, name: 'Superman' },
+    { id: 14, name: 'Magneto'},
+    { id: 15, name: 'Wolverine'}
+];
